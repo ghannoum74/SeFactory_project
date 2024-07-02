6575737d41fa0640bd62023692d2d630d7e37d92
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-02 21:15:38 +0300

    create gender, age, nationality element and change address data to nationality

--- project_abdelrahman_ghannoum.py
+++ project_abdelrahman_ghannoum.py
@@ -1,8 +1,10 @@
 import tkinter
+from tkinter import ttk
 class UserNode:
     def __init__(self,friendsData) -> None:
         self.fullname = friendsData["fullname"]
         self.age = friendsData["age"]
+        self.gender = friendsData["gender"]
         self.email = friendsData["email"] 
         self.password = friendsData["password"]
         self.address = friendsData["address"] 
@@ -63,6 +65,7 @@ class FriendsList:
                 friend_data = {
                     "fullname" : current.fullname,
                     "age" : current.age,
+                    "gender" : current.gender,
                     "email" : current.email,
                     "password" : current.password,
                     "address" : current.address,
@@ -91,9 +94,38 @@ class FriendshipCommunity:
         user_info_frame = tkinter.LabelFrame(frame, text= "User_Information")
         user_info_frame.grid(row=0 , column= 0 )
 
-#create new wedget
+#create full_name label
         full_name_label = tkinter.Label(user_info_frame, text="Full name")
+#render label on the screen
         full_name_label.grid(row = 0, column = 0)
+#create the input for full_name label
+        full_name_input = tkinter.Entry(user_info_frame)
+#render input on the screen
+        full_name_input.grid(row = 1, column= 0,padx=20,pady=20)
+#create gender label
+        gender = tkinter.Label(user_info_frame, text="Gender")
+#render label
+        gender.grid(row= 0, column= 2 )
+#create combobox
+        gender_combobox = ttk.Combobox(user_info_frame, values=["Male", "Female", "rather not say"])
+#render combobox
+        gender_combobox.grid(row=1, column= 2)
+#create age_label
+        age_label = tkinter.Label(user_info_frame, text="Age")
+#render age_label
+        age_label.grid(row=0 , column=3)
+#create age spinbox
+        age_spinbox = tkinter.Spinbox(user_info_frame, from_=12 , to= 90)
+#render age spinbox
+        age_spinbox.grid(row=1, column=3)
+#create address label
+        address_label = tkinter.Label(user_info_frame, text="Address")
+#render address label
+        address_label.grid(row=2, column=0)
+#create address input
+        address_input = tkinter.Entry(user_info_frame)
+#render address input
+        address_input.grid(row=3, column=0)
 #to run my window component
         window.mainloop()
 
