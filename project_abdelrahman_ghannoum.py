a0e4b2dd8fa6ba9cb68da41942ecd9af6b467126
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-02 16:50:08 +0300

    create FriendList and create addFriend method

--- project_abdelrahman_ghannoum.py
+++ project_abdelrahman_ghannoum.py
@@ -1,17 +1,34 @@
 import tkinter
-class User:
-    def __init__(self, fullname, age, email, password, address, bio , hobit) -> None:
+import json
+class UserNode:
+    def __init__(self,fullname, age, email, password, address, hobits, bio) -> None:
         self.fullname = fullname
         self.age = age
         self.email = email 
         self.password = password
         self.address = address 
-        self.hobit = hobit
+        self.hobits = hobits
         self.bio = bio
         self.following = 0
         self.followers = 0
         self.block = 0
         self.isActive = False
-        self.isAdmin = False
+        # self.isAdmin = False
+        pass
+#It's our linked list for the friends of vertesis
+class FiendsList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def addFriend(self, friendData):
+        friend = UserNode(friendData)
+        friend.next = self.head
+        self.head = friend
+        self.size += 1
+
+
+    
 
    
