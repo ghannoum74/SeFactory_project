afb9e4a1dbd9464ebb9c795eb4602d4435509fbe
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-04 20:29:59 +0300

    create follow function and modify userNode constactur to accept only email

--- project_abdelrahman_ghannoum.py
+++ project_abdelrahman_ghannoum.py
@@ -1,15 +1,15 @@
 # from addNewUser import addUser
 from newUserClass import newUSer
 class UserNode:
-    def __init__(self,friendsData) -> None:
-        self.fullname = friendsData["fullname"]
-        self.age = friendsData["age"]
-        self.gender = friendsData["gender"]
-        self.email = friendsData["email"] 
-        self.password = friendsData["password"]
-        self.nationality = friendsData["nationality"] 
-        self.hobbies= friendsData["hobbies"]
-        self.bio = friendsData["bio"]
+    def __init__(self, email) -> None:
+        # self.fullname = friendsData["fullname"]
+        # self.age = friendsData["age"]
+        # self.gender = friendsData["gender"]
+        # self.password = friendsData["password"]
+        # self.nationality = friendsData["nationality"] 
+        # self.hobbies= friendsData["hobbies"]
+        # self.bio = friendsData["bio"]
+        self.email = email
         self.following = 0
         self.followers = 0
         self.block = 0
@@ -28,7 +28,7 @@ class FriendsList:
         friend = UserNode(friendData)
         friend.next = self.head
         self.head = friend
-        print(f"{friendData["fullname"] } has been added successfuly")
+        print(f"{friendData} has been added successfuly")
         self.size += 1
 
 #it's the same as removeNode
@@ -81,19 +81,40 @@ class FriendsList:
 class FriendshipCommunity:
     def __init__(self) -> None:
         self.adj_list = {}
+        
 
     def createUser(self):
         user = newUSer()
         user.addUser()
+        
     #i already managed the existence user in the addUser()function so all user is not existe in the self.adj_list
     #this is like temparary data base when we enter the user GUI you can add as match as you want user and it managed if user entered twice 
     #when you print exit all users entered is passed to user object and store it in the adj_list dictionary
     #so now i retrive each user (email) and set it as vertex with value equal to empty FriendList[linledlist]
-        print(user.user_data)
-        if user.user_data.keys not in self.adj_list:
-            self.adj_list[user.user_data.keys] = FriendsList() 
+        users_email = list(user.user_data.keys())
+        for email in users_email:
+            self.adj_list[email] = FriendsList()
 
     
+    def follow(self, sourceUser, destinationUser):
+        if sourceUser in self.adj_list and destinationUser in self.adj_list:
+            self.adj_list[sourceUser].addFriend(destinationUser)
+            print(f"You are now friend with {destinationUser}")
+        elif sourceUser not in self.adj_list and destinationUser not in self.adj_list:
+            print("Invalid", sourceUser,"and",destinationUser)
+        elif sourceUser not in self.adj_list:
+            print("Invalid", sourceUser)
+        elif destinationUser not in self.adj_list:
+            print("Invalid",destinationUser)
+
+    def display(self):
+        if self.adj_list == {}:
+            print("Graph is empty!\n")
+            return
+        for vertex in self.adj_list:
+            friends_str = self.adj_list[vertex].displayFriends()
+            print(f"{vertex}: {friends_str}")
+
     
 
         
@@ -101,6 +122,6 @@ class FriendshipCommunity:
 def main():
     friend = FriendshipCommunity()
     friend.createUser()
-    
+    friend.follow("aboud1@gmail.com", "aboud2@gmail.com")
 main()
     
